b12173023098070d8c0ebc1dd195b6db925f004e
alexgorkoff <alex.gorkoff29@gmail.com>
2019-04-02 15:00:38 -0600

    Completed JavaDocs for class Inventory

--- ENSF409_ProjectMilestone2/src/serverModel/Inventory.java
+++ ENSF409_ProjectMilestone2/src/serverModel/Inventory.java
@@ -1,24 +1,52 @@
 package serverModel;
 import java.util.ArrayList;
 
+/**
+ * Class Inventory 
+ * 
+ * @author Dr. Mohamed Moshirpour (Retrieved Sunday March 31st, 2019)
+ * Edited by: 
+ * @author Muhammad Farooq (UCID: 30016276)
+ * @author Alexander Gorkoff (UCID: 30020570)
+ * @author Matteo Messana (UCIDL 30020933)
+ *
+ */
 public class Inventory {
 	
 	private ArrayList <Item> itemList;
 	private Order myOrder;
 	
-	
+	/**
+	 * Constructor for class inventory
+	 * @param itemList : An ArrayList containing all items in the inventory
+	 */
 	public Inventory (ArrayList <Item> itemList) {
 		this.itemList = itemList;
 		myOrder = new Order ();
 	}
 
+	/**
+	 * Returns a reference to the inventory Item List
+	 * @return itemList: an ArrayList containing all items in the inventory
+	 */
 	public ArrayList <Item> getItemList() {
 		return itemList;
 	}
 
+	/**
+	 * Sets the inventory Item List
+	 * @param itemList : an ArrayList containing teh inventory for the store
+	 */
 	public void setItemList(ArrayList <Item> itemList) {
 		this.itemList = itemList;
 	}
+	
+	/**
+	 * Invokes method to decrease item (if prompted by user)
+	 * If item quantity decreases below 40, automatically places an order for said item
+	 * @param name : The name of the item inputed by the user
+	 * @return theItem : An instance of the item (if the quantity has decreased)
+	 */
 	public Item manageItem (String name){
 		Item theItem = decreaseItem (name);
 		
@@ -27,12 +55,25 @@ public class Inventory {
 		}
 		return theItem;
 	}
+	
+	/**
+	 * Invokes method to place an order for a given Item
+	 * If an item is returned, an OrderLine for that Item is created and added to the day's Order
+	 * @param theItem : The item to be ordered
+	 */
 	public void placeOrder (Item theItem){
 		OrderLine ol = theItem.placeOrder();
 		if (ol !=null){
 			myOrder.addOrderLine(ol);
 		}
 	}
+	
+	/**
+	 * Searches for an Item as indicated by the user
+	 * Attempts to decrease the quantity of the item (if applicable)
+	 * @param name : The name of the item to be searched
+	 * @return theItem (if the item was found and quantity was decreased) or null (if the item was not located or could not be decreased
+	 */
 	private Item decreaseItem (String name) {
 		
 		Item theItem = searchForItem (name);
@@ -48,6 +89,11 @@ public class Inventory {
 		
 	}
 	
+	/**
+	 * Returns the quantity of an Item as indicated by the user
+	 * @param name : The name of the item to be searched
+	 * @return -1 if the item could not be located or the quantity of the item that has been located
+	 */
 	public int getItemQuantity (String name){
 		Item theItem = searchForItem (name);
 		if (theItem == null)
@@ -55,6 +101,12 @@ public class Inventory {
 		else
 			return theItem.getItemQuantity();
 	}
+	
+	/**
+	 * Search for an Item in the inventory by name
+	 * @param name : The name of the item to be searched
+	 * @return theItem (if located in search) or null (if not located in search)
+	 */
 	public Item searchForItem (String name) {
 		for (Item i: itemList) {
 			if (i.getItemName().equals(name))
@@ -71,6 +123,11 @@ public class Inventory {
 		return str;
 	}
 
+	/**
+	 * Search for an Item in the inventory by ID
+	 * @param id : The ID of the item to be searched
+	 * @return the Item (if located in the search) or null (if not located in the search)
+	 */
 	public Item searchForItem(int id) {
 		// TODO Auto-generated method stub
 		for (Item i: itemList) {
@@ -80,6 +137,10 @@ public class Inventory {
 		return null;
 	}
 
+	/**
+	 * Prints the Order for the day
+	 * @return the Order (as a String)
+	 */
 	public String printOrder() {
 		// TODO Auto-generated method stub
 		return myOrder.toString();
