fd2cd26969ba81fa4902544cb413b3a20b427502
Matteo-Messana <mnm@italtech.ca>
2019-04-03 13:45:47 -0600

    Comments on database class

--- ENSF409_ProjectMilestone2/src/serverModel/Database.java
+++ ENSF409_ProjectMilestone2/src/serverModel/Database.java
@@ -1,18 +1,33 @@
 package serverModel;
 
 import java.sql.*;
+/**
+ * A class that configures the data from the database
+ * and makes it usable for the rest of the program.
+ * @author Matteo Messana, Alexander Gorkoff, Usman Farooq
+ *
+ */
 
 public class Database {
+	/**
+	 * Member variables for class Database
+	 */
 	private Connection myConnection;
 	private Statement myStatement;
 	private ResultSet supplierResult;
 	private ResultSet itemResult;
 	
+	/**
+	 * Constructor for Database. 
+	 * Initializes all member variables for the Database.
+	 */
 	public Database()
 	{
 		try {
 			// Get a connection to database
 			myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/toolshop", "ToolShopUser", "ToolShop2019");
+			
+			
 			
 			// Create a statement
 			myStatement = myConnection.createStatement();
@@ -22,12 +37,15 @@ public class Database {
 			itemResult = myStatement.executeQuery("select * from items");
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	/*public void processResult() throws SQLException
+	/**
+	 * Function that processes the data in both the tables.
+	 * @throws SQLException
+	 */
+	public void processResult() throws SQLException
 	{
 		while(supplierResult.next())
 		{
@@ -35,9 +53,4 @@ public class Database {
 		}
 	}
 	
-	public static void main(String args[]) throws SQLException
-	{
-		Database D = new Database();
-		D.processResult();
-	}*/
 }
